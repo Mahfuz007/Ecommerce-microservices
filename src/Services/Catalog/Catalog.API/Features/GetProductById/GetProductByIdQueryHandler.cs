ef0d1ec52e34fe21ba3417579d205faaf5932c5f
Mahfuz <aamahfuz2@gmail.com>
2024-04-27 01:15:47 +0600

    GetProductById APi feature implemented

---
+++ src/Services/Catalog/Catalog.API/Features/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Models;
+using CommonBlocks.CQRS;
+using Marten;
+
+namespace Catalog.API.Features.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+public class GetProductByIdQueryHandler(IDocumentSession _session) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        var result = await _session.LoadAsync<Product>(query.Id, cancellationToken);
+        return new GetProductByIdResult(result);
+    }
+}
