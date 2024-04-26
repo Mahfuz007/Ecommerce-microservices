e71bff2b702e16d83b4e023722f46c6f13ffcba9
Mahfuz <aamahfuz2@gmail.com>
2024-04-27 00:54:16 +0600

    GetAllProducts feature added

---
+++ src/Services/Catalog/Catalog.API/Features/GetAllProducts/GetAllProductsEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Carter;
+using Catalog.API.Features.CreateProduct;
+using Catalog.API.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Features.GetAllProducts;
+
+//public record GetAllProductsRequest()
+public record GetAllProductResponse(List<Product> Products);
+public class GetAllProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetAllProductsQuery());
+            var response = result.Adapt(result.Products);
+            return response;
+        })
+          .WithName("GetAllProducts")
+         .Produces<GetAllProductResponse>(StatusCodes.Status200OK)
+         .ProducesProblem(StatusCodes.Status500InternalServerError)
+         .WithDescription("Get All Products")
+         .WithSummary("Get All Products");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Features/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.API.Models;
+using CommonBlocks.CQRS;
+using Marten;
+
+namespace Catalog.API.Features.GetAllProducts;
+
+public record GetAllProductsQuery() : IQuery<GetAllProductsResult>;
+public record GetAllProductsResult(List<Product> Products);
+internal class GetAllProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetAllProductsQuery, GetAllProductsResult>
+{
+    public async Task<GetAllProductsResult> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+    {
+        var result = await session.Query<Product>().ToListAsync(cancellationToken);
+        return new GetAllProductsResult((List<Product>)result);
+    }
+}
