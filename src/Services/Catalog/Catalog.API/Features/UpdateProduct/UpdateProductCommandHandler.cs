59cbbe9626513bb0d38481ffacd18abdff1a2004
Mahfuz <aamahfuz2@gmail.com>
2024-04-27 10:27:46 +0600

    Update Product Api Implemented

---
+++ src/Services/Catalog/Catalog.API/Features/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.API.Models;
+using CommonBlocks.CQRS;
+using Marten;
+
+namespace Catalog.API.Features.UpdateProduct;
+
+public record UpateProductCommand(Guid Id, string Name, string Description, List<string> Category, string ImageFileUrl, decimal Price) : ICommand<UpdateProductResult>;
+public record UpdateProductResult(bool IsSuccess);
+
+public class UpdateProductCommandHandler(IDocumentSession _session) : ICommandHandler<UpateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Id = command.Id,
+            Name = command.Name,
+            Description = command.Description,
+            Category = command.Category,
+            ImageFileUrl = command.ImageFileUrl,
+            Price = command.Price
+        };
+
+        _session.Store<Product>(product);
+        await _session.SaveChangesAsync(cancellationToken);
+        return new UpdateProductResult(true);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Features/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Features.UpdateProduct;
+
+public record UpateProductRequest(Guid Id, string Name, string Description, List<string> Category, string ImageFileUrl, decimal Price);
+public record UpdateProductResponse(bool IsSuccess);
+
+public class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("products", async (UpateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<UpdateProductResponse>();
+            return Results.Ok(response);
+        })
+         .WithName("UpateProduct")
+         .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+         .ProducesProblem(StatusCodes.Status500InternalServerError)
+         .WithDescription("Upate Product")
+         .WithSummary("Upate Product");
+    }
+}
