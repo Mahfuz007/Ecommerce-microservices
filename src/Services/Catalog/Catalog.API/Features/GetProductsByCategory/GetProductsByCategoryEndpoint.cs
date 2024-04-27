41cda2b574c9dd56c1d04b8b2b23e9986052407f
Mahfuz <aamahfuz2@gmail.com>
2024-04-27 10:11:52 +0600

    GetProductsByCategory Api implemented

---
+++ src/Services/Catalog/Catalog.API/Features/GetProductsByCategory/GetProductsByCategoryEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Carter;
+using Catalog.API.Models;
+using CommonBlocks.CQRS;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Features.GetProductsByCategory;
+
+public record GetProductsByCategoryRequest(string Category) : IQuery<GetProductsByCategoryResponse>;
+public record GetProductsByCategoryResponse(IEnumerable<Product> Products);
+public class GetProductsByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("products/category/{category}", async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsByCategoryQuery(category));
+            var resonse = result.Adapt<GetProductsByCategoryResponse>();
+            return Results.Ok(resonse);
+        })
+          .WithName("GetProductsByCategory")
+          .Produces<GetProductsByCategoryResponse>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status500InternalServerError)
+          .WithDescription("Get Products by Category")
+          .WithSummary("Get Products by Category");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Features/GetProductsByCategory/GetProductsByCategoryQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Models;
+using CommonBlocks.CQRS;
+using Marten;
+
+namespace Catalog.API.Features.GetProductsByCategory;
+
+public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductsByCategoryQueryHandler(IDocumentSession _session) : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        var result = await _session.Query<Product>().Where(x => x.Category.Contains(query.Category)).ToListAsync();
+        return new GetProductsByCategoryResult(result);
+    }
+}
