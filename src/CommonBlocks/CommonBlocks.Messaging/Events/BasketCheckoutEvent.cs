f0e61d641eaec09aa261edcf27fb3ffdc7074b09
Mahfuz <aamahfuz2@gmail.com>
2024-05-12 13:38:39 +0600

    Common blocks messaging project added and mastransite rabbitMq configuartion added

---
+++ src/CommonBlocks/CommonBlocks.Messaging/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,26 @@
+﻿namespace CommonBlocks.Messaging.Events;
+
+public record BasketCheckoutEvent : IntegrationEvent
+{
+    public string UserName { get; set; } = default!;
+    public Guid CustomerId { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    //Shipping and Billing Addess
+
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string? Email { get; set; }
+    public string AddressLine { get; set; } = default!;
+    public string City { get; set; } = default!;
+    public string State { get; set; } = default!;
+    public string ZipCode { get; set; } = default!;
+    public string Country { get; set; } = default!;
+
+    //Payment information
+    public string Name { get; set; } = default!;
+    public string CardNumber { get; set; } = default!;
+    public string CVV { get; set; } = default!;
+    public string ExpiryDate { get; set; } = default!;
+    public int PaymentMethod { get; set; }
+}
---
+++ src/CommonBlocks/CommonBlocks.Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace CommonBlocks.Messaging.Events;
+
+public record IntegrationEvent
+{
+    Guid EventId => Guid.NewGuid();
+    DateTime EvenetOccuredOn => DateTime.UtcNow;
+    string EventType => GetType().AssemblyQualifiedName!;
+}
